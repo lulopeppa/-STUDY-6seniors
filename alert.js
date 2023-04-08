0bef35ec550e363f9f0635f87d14559b16075243
Nikolay <nick.allekseev@gmail.com>
2023-04-08 20:47:12 +0300

    Задачи

--- alert.js
+++ alert.js
@@ -1,6 +1,23 @@
-//Переменные - решение задач
-let admin;
-let name;
-name = 'Джон' // почему name тут перечеркнуто?
-admin = name;
-alert (admin)
+//Взаимодействие: alert, prompt, confirm
+
+// let userName = prompt('Как тебя зовут?',"Ваше имя");
+
+// alert(userName);
+
+// let notBot = confirm ("ты бот?", "");
+// alert (notBot);
+
+/*
+//Преобразование типов
+
+let value = true;
+alert(typeof value); // boolean
+
+value = String(value); // теперь value это строка "true"
+alert(typeof value); // string
+*/
+
+let a = prompt("Первое число?", 1);
+let b = prompt("Второе число?", 2);
+
+alert (+a + +b);
